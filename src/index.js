186dc557e5781fbd4cb58bb38d20de0daf6c0385
valentinancy <nancy_valentina@ymail.com>
2017-02-05 16:12:53 +0700

    full function without redux

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
+import YTSearch from 'youtube-api-search'
+
+const API_KEY = 'AIzaSyD6qE1zXTAmpp0do4tFqoQ_kkvf38rKTcw'
+
+class App extends Component {
+    
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        }
+
+        YTSearch({key: API_KEY, term: 'makeup tutorial'}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
+        })
+    }
+    
+    render() {
+        return(
+            <div>
+                <SearchBar />
+                <VideoDetail video={ this.state.selectedVideo }/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({ selectedVideo })} 
+                    videos={ this.state.videos } />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
